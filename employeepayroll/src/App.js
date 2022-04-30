4555e96c531de5baa2a9023f08165778f7293d52
ALLOJU <mounika.alloju@gmail.com>
2022-04-30 09:48:06 +0530

    Employee payroll with payroll and dashboard added

---
+++ employeepayroll/src/App.js
@@ -0,0 +1,31 @@
+
+import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from "react-router-dom";
+import PayrollForm from './components/payroll-form/PayrollForm';
+import DashBoard from './components/payroll-form/DashBoard';
+
+
+function App() {
+  return (
+    
+    <Router>
+    <div className="App">
+
+      <Switch>
+        
+        <Route exact path="/" component={DashBoard}/>
+        <Route exact path="/payroll" component={PayrollForm}/> 
+        
+      </Switch>
+    </div>
+  </Router>
+    
+  );
+}
+
+export default App;
